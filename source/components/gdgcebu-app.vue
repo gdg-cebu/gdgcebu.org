3c1c6c2dbb6a947dc7a9b9817e9d19d4d1cbbc53
Arnelle Balane <arnellebalane@gmail.com>
2018-02-28 06:44:43 +0800

    feat(app): render app-header in top-level component

--- source/components/gdgcebu-app.vue
+++ source/components/gdgcebu-app.vue
@@ -1,3 +1,14 @@
 <template>
-    <router-view></router-view>
+    <main class="gdgcebu-app">
+        <app-header></app-header>
+        <router-view></router-view>
+    </main>
 </template>
+
+<script>
+    export default {
+        components: {
+            'app-header': require('./app-header.vue').default
+        }
+    };
+</script>
