b37b17a52a82507b634a89be1c573ba1813c8b92
Arnelle Balane <arnellebalane@gmail.com>
2018-02-28 07:22:05 +0800

    feat(app): add what we do section in landing-page

--- source/components/landing-page.vue
+++ source/components/landing-page.vue
@@ -1,13 +1,15 @@
 <template>
     <div class="landing-page">
         <about-us></about-us>
+        <what-we-do></what-we-do>
     </div>
 </template>
 
 <script>
     export default {
         components: {
-            'about-us': require('./landing-page/about-us.vue').default
+            'about-us': require('./landing-page/about-us.vue').default,
+            'what-we-do': require('./landing-page/what-we-do.vue').default
         }
     };
 </script>
---
+++ source/components/landing-page/what-we-do.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="what-we-do">
+        <div class="wrapper">
+            <h1>What we do</h1>
+
+            <ul>
+                <li>
+                    <div class="icon-placeholder"></div>
+                    <h2>Talks</h2>
+                    <p>Get updated with the latest news and announcements</p>
+                </li>
+                <li>
+                    <div class="icon-placeholder"></div>
+                    <h2>Codelabs</h2>
+                    <p>Get hands-on activity and mentors to guide you</p>
+                </li>
+                <li>
+                    <div class="icon-placeholder"></div>
+                    <h2>Roadshows</h2>
+                    <p>Share knowledge in different colleges and universities</p>
+                </li>
+                <li>
+                    <div class="icon-placeholder"></div>
+                    <h2>Live Viewing Parties</h2>
+                    <p>Watch a live Google event happening in different countries</p>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .what-we-do {
+        padding: 7rem 0;
+    }
+
+    h1 {
+        margin: 0;
+        margin-bottom: 5rem;
+        font-size: 2.4rem;
+        font-weight: 500;
+        text-align: center;
+    }
+
+    ul {
+        display: flex;
+        flex-wrap: wrap;
+        padding: 0;
+        margin: 0;
+        list-style: none;
+    }
+
+    li {
+        flex: 1 0 0;
+        min-width: 29rem;
+        text-align: center;
+    }
+
+    li:not(:first-child) {
+        margin-left: 0.6rem;
+    }
+
+    li:not(:last-child) {
+        margin-right: 0.6rem;
+    }
+
+    .icon-placeholder {
+        width: 5rem;
+        height: 5rem;
+        border-radius: 50%;
+        margin: auto;
+        background-color: #eee;
+    }
+
+    h2 {
+        font-size: 1.8rem;
+        font-weight: 500;
+    }
+
+    p {
+        color: #888;
+    }
+</style>
