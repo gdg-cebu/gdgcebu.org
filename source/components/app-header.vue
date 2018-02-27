17cd6535ffd26534d1bbf5815b2585c1d71fd99c
Arnelle Balane <arnellebalane@gmail.com>
2018-02-28 06:44:14 +0800

    feat(app): define app-header component

---
+++ source/components/app-header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header class="app-header">
+        <div class="wrapper">
+            <router-link :to="{ name: 'landing-page' }">
+                <img src="~images/gdg-logo.png" alt="GDG Logo">
+            </router-link>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+    .app-header {
+        height: 5.6rem;
+        border-bottom: 1px solid #ddd;
+    }
+
+    .wrapper {
+        display: flex;
+        align-items: center;
+        height: 100%;
+    }
+</style>
