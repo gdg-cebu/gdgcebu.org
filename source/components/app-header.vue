9bafc9d60c433165b50d7d69ff40cf56f4b0c699
Arnelle Balane <arnellebalane@gmail.com>
2018-02-28 06:50:24 +0800

    feat(app): add nav links to app-header

--- source/components/app-header.vue
+++ source/components/app-header.vue
@@ -1,9 +1,15 @@
 <template>
     <header class="app-header">
         <div class="wrapper">
-            <router-link :to="{ name: 'landing-page' }">
+            <router-link :to="{ name: 'landing-page' }" class="app-logo">
                 <img src="~images/gdg-logo.png" alt="GDG Logo">
             </router-link>
+
+            <nav>
+                <router-link :to="{ name: 'landing-page' }">About</router-link>
+                <router-link :to="{ name: 'landing-page' }">Events</router-link>
+                <router-link :to="{ name: 'landing-page' }">Team</router-link>
+            </nav>
         </div>
     </header>
 </template>
@@ -18,5 +24,20 @@
         display: flex;
         align-items: center;
         height: 100%;
+    }
+
+    .app-logo {
+        margin-right: 7rem;
+    }
+
+    nav {
+        font-size: 0;
+    }
+
+    nav a {
+        display: inline-block;
+        padding: 0 2.4rem;
+        font-size: 1.5rem;
+        text-decoration: none;
     }
 </style>
