538e8c248a1ee17fc3f79d3f5e267300860a8712
Arnelle Balane <arnellebalane@gmail.com>
2018-03-01 01:03:09 +0800

    feat(app): store events data in vuex store

--- source/store.js
+++ source/store.js
@@ -3,7 +3,34 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
-const state = {};
+const state = {
+    events: [ {
+        title: 'Mobile Sites Certification: University of the Philippines Cebu',
+        date: 'March 14, 2018',
+        venue: 'UP Cebu',
+        description: 'GDG Cebu\'s biggest event yet. The event will focus on essential technical topics for developers, along with updates on the current development trends. The topics will range from Machine Learning, Firebase, and the Cloud, to Material Design, Android, and 2017\'s Web Trends. Attendees will be learning about these topics from Googlers themselves, who will be flying in from South Korea, Japan, Singapore and the US.',
+        image: ''
+    }, {
+        title: 'Mobile Sites Certification: University of the Philippines Cebu',
+        date: 'March 14, 2018',
+        venue: 'UP Cebu',
+        description: 'GDG Cebu\'s biggest event yet. The event will focus on essential technical topics for developers, along with updates on the current development trends. The topics will range from Machine Learning, Firebase, and the Cloud, to Material Design, Android, and 2017\'s Web Trends. Attendees will be learning about these topics from Googlers themselves, who will be flying in from South Korea, Japan, Singapore and the US.',
+        image: ''
+    }, {
+        title: 'Mobile Sites Certification: University of the Philippines Cebu',
+        date: 'March 14, 2018',
+        venue: 'UP Cebu',
+        description: 'GDG Cebu\'s biggest event yet. The event will focus on essential technical topics for developers, along with updates on the current development trends. The topics will range from Machine Learning, Firebase, and the Cloud, to Material Design, Android, and 2017\'s Web Trends. Attendees will be learning about these topics from Googlers themselves, who will be flying in from South Korea, Japan, Singapore and the US.',
+        image: ''
+    }, {
+        title: 'Mobile Sites Certification: University of the Philippines Cebu',
+        date: 'March 14, 2018',
+        venue: 'UP Cebu',
+        description: 'GDG Cebu\'s biggest event yet. The event will focus on essential technical topics for developers, along with updates on the current development trends. The topics will range from Machine Learning, Firebase, and the Cloud, to Material Design, Android, and 2017\'s Web Trends. Attendees will be learning about these topics from Googlers themselves, who will be flying in from South Korea, Japan, Singapore and the US.',
+        image: ''
+    } ]
+};
+
 const mutations = {};
 const actions = {};
 
