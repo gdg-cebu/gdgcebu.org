6e5fb41b06afccf0b975f706f4b2612bdf6cb4c6
Arnelle Balane <arnellebalane@gmail.com>
2018-03-01 01:24:21 +0800

    feat(app): render team data in team-page

--- source/components/team-page.vue
+++ source/components/team-page.vue
@@ -4,12 +4,28 @@
             <div class="page-header">
                 <h1>The Team</h1>
             </div>
+
+            <div class="team">
+                <team-item v-for="(member, i) in team" :key="i" :data="member"></team-item>
+            </div>
         </div>
     </div>
 </template>
 
+<script>
+    import { mapState } from 'vuex';
+
+    export default {
+        computed: mapState(['team']),
+
+        components: {
+            'team-item': require('./team-page/team-item.vue').default
+        }
+    };
+</script>
+
 <style scoped>
-    .events-page {
+    .team-page {
         padding-bottom: 20rem;
     }
 
@@ -20,5 +36,15 @@
     h1 {
         font-size: 2.4rem;
         font-weight: 500;
+    }
+
+    .team {
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .team-item {
+        width: 39rem;
+        padding: 3.2rem;
     }
 </style>
---
+++ source/components/team-page/team-item.vue
@@ -0,0 +1,36 @@
+<template>
+    <article class="team-item">
+        <img :src="data.image" :alt="data.name">
+        <h2>{{ data.name }}</h2>
+        <p>{{ data.role }}</p>
+    </article>
+</template>
+
+<script>
+    export default {
+        props: ['data']
+    };
+</script>
+
+<style scoped>
+    .team-item {
+        text-align: center;
+    }
+
+    img {
+        width: 10rem;
+        height: 10rem;
+        border-radius: 50%;
+        object-fit: cover;
+        object-position: center center;
+    }
+
+    h2 {
+        font-size: 1.8rem;
+        font-weight: 500;
+    }
+
+    p {
+        color: #888;
+    }
+</style>
