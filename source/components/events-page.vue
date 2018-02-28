1ec8a6f700b092fdfbe980a3f23eef27aa75ae37
Arnelle Balane <arnellebalane@gmail.com>
2018-03-01 00:56:37 +0800

    feat(app): display list of event items

--- source/components/events-page.vue
+++ source/components/events-page.vue
@@ -1,5 +1,47 @@
 <template>
     <div class="events-page">
-        <h1>Events Page</h1>
+        <div class="wrapper">
+            <div class="page-header">
+                <h1>Our Events</h1>
+            </div>
+
+            <div class="events">
+                <event-item></event-item>
+                <event-item></event-item>
+                <event-item></event-item>
+                <event-item></event-item>
+            </div>
+        </div>
     </div>
 </template>
+
+<script>
+    export default {
+        components: {
+            'event-item': require('./events-page/event-item.vue').default
+        }
+    };
+</script>
+
+<style scoped>
+    .events-page {
+        padding-bottom: 20rem;
+    }
+
+    .page-header {
+        padding: 7rem 0;
+    }
+
+    h1 {
+        font-size: 2.4rem;
+        font-weight: 500;
+    }
+
+    .event-item:not(:first-child) {
+        margin-top: 3.6rem;
+    }
+
+    .event-item:not(:last-child) {
+        margin-bottom: 3.6rem;
+    }
+</style>
