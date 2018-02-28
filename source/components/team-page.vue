9d1b4d798080d680460bdc69edefd3f2813604bd
Arnelle Balane <arnellebalane@gmail.com>
2018-03-01 01:07:58 +0800

    feat(app): initial team-page component

---
+++ source/components/team-page.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="team-page">
+        <div class="wrapper">
+            <div class="page-header">
+                <h1>The Team</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .events-page {
+        padding-bottom: 20rem;
+    }
+
+    .page-header {
+        padding: 7rem 0;
+    }
+
+    h1 {
+        font-size: 2.4rem;
+        font-weight: 500;
+    }
+</style>
