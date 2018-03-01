bf2f4315a04810933027b9101bd707dadb6c491d
Julia Menchavez <jcmenchavez@gmail.com>
2018-02-28 19:45:26 -0800

    added events, team

--- source/store.js
+++ source/store.js
@@ -5,36 +5,66 @@ Vue.use(Vuex);
 
 const state = {
     upcomingEvent: {
-        title: 'Mobile Sites Certification: University of the Philippines Cebu',
-        date: 'March 14, 2018 1:00-5:30 PM',
-        venue: 'RM 302-306, UP Cebu',
-        description: 'GDG Cebu\'s biggest event yet. The event will focus on essential technical topics for developers, along with updates on the current development trends. The topics will range from Machine Learning, Firebase, and the Cloud, to Material Design, Android, and 2017\'s Web Trends. Attendees will be learning about these topics from Googlers themselves, who will be flying in from South Korea, Japan, Singapore and the US.',
+        title: 'Study Jams 2018',
+        date: 'TBA',
+        venue: 'TBA',
+        description: 'This event aims to certify developers with the Google Mobile Sites Certification. We will be holding the certification and exclusive in depth talks to assist in passing the certification in a limited number of schools. Join us now!',
         image: 'https://picsum.photos/500/500'
     },
 
     events: [ {
-        title: 'Mobile Sites Certification: University of the Philippines Cebu',
-        date: 'March 14, 2018',
-        venue: 'UP Cebu',
-        description: 'GDG Cebu\'s biggest event yet. The event will focus on essential technical topics for developers, along with updates on the current development trends. The topics will range from Machine Learning, Firebase, and the Cloud, to Material Design, Android, and 2017\'s Web Trends. Attendees will be learning about these topics from Googlers themselves, who will be flying in from South Korea, Japan, Singapore and the US.',
+        title: 'Women Tech Makers Cebu 2018',
+        date: 'TBA',
+        venue: 'TBA',
+        description: 'It\'s International Womens Month! Google celebrates globally the Women Techmakers Event Series around International Women\'s Day. Women Techmakers events focus on a technical women audience, showcase female technical talent, and encourage attendees to network and empower each other in the field of technology. WTM events achieve these objectives by including event components that fall under the buckets: Technical, Community, Inspirational, Leadership. Below are examples of what these buckets could include, and by no means are an exhaustive list. We encourage you to be creative and consider the resources available when choosing what event components to include in your events.',
         image: 'https://picsum.photos/200/200'
     }, {
-        title: 'Mobile Sites Certification: University of the Philippines Cebu',
-        date: 'March 14, 2018',
-        venue: 'UP Cebu',
-        description: 'GDG Cebu\'s biggest event yet. The event will focus on essential technical topics for developers, along with updates on the current development trends. The topics will range from Machine Learning, Firebase, and the Cloud, to Material Design, Android, and 2017\'s Web Trends. Attendees will be learning about these topics from Googlers themselves, who will be flying in from South Korea, Japan, Singapore and the US.',
+        title: 'Cloud Next Extended Cebu 2018',
+        date: 'TBA',
+        venue: 'TBA',
+        description: 'GCP Next Extended in Cebu and join the largest gathering of the Google Cloud Platform community to explore the latest developments in cloud technology.',
         image: 'https://picsum.photos/200/200'
     }, {
-        title: 'Mobile Sites Certification: University of the Philippines Cebu',
-        date: 'March 14, 2018',
-        venue: 'UP Cebu',
-        description: 'GDG Cebu\'s biggest event yet. The event will focus on essential technical topics for developers, along with updates on the current development trends. The topics will range from Machine Learning, Firebase, and the Cloud, to Material Design, Android, and 2017\'s Web Trends. Attendees will be learning about these topics from Googlers themselves, who will be flying in from South Korea, Japan, Singapore and the US.',
+        title: 'Firebase Summit 2018',
+        date: 'TBA',
+        venue: 'TBA',
+        description: '',
         image: 'https://picsum.photos/200/200'
     }, {
-        title: 'Mobile Sites Certification: University of the Philippines Cebu',
-        date: 'March 14, 2018',
-        venue: 'UP Cebu',
-        description: 'GDG Cebu\'s biggest event yet. The event will focus on essential technical topics for developers, along with updates on the current development trends. The topics will range from Machine Learning, Firebase, and the Cloud, to Material Design, Android, and 2017\'s Web Trends. Attendees will be learning about these topics from Googlers themselves, who will be flying in from South Korea, Japan, Singapore and the US.',
+        title: 'TensorFlow Dev Summit 2018',
+        date: 'TBA',
+        venue: 'TBA',
+        description: '',
+        image: 'https://picsum.photos/200/200'
+    }, {
+        title: 'Google Assistant Cebu Meetup 2018',
+        date: 'TBA',
+        venue: 'TBA',
+        description: '',
+        image: 'https://picsum.photos/200/200'
+    }, {
+        title: 'Campus Talks 2018',
+        date: 'TBA',
+        venue: 'TBA',
+        description: 'This event is composed of talks and codelab sessions which aims to assist the students to be fully equipped with the latest trend of technology developed by Google fundamentally focusing on the programming and development field.',
+        image: 'https://picsum.photos/200/200'
+    }, {
+        title: 'DevMeets 2018',
+        date: 'TBA',
+        venue: 'TBA',
+        description: 'GDG DevMeets is the newest program of GDG Cebu to help developers learn more about specific technologies. Every 3rd Friday of the month, we will be doing a hands-on workshop on a specific technology.',
+        image: 'https://picsum.photos/200/200'
+    }, {
+        title: 'Google I/O Extended Cebu 2018',
+        date: 'TBA',
+        venue: 'TBA',
+        description: 'The event is composed of a series of keynotes, talks, Codelabs, and workshops organized by Google Developers Group Cebu (GDG Cebu), a non-profit group of amateurs and professionals in Cebu’s developer community who are passionate about Google technologies',
+        image: 'https://picsum.photos/200/200'
+    }, {
+        title: 'GDG Cebu Devfest 2018',
+        date: 'TBA',
+        venue: 'TBA',
+        description: 'The biggest Google tech conference in Cebu carefully crafted for you by GDG community! All about Android, Web and Cloud from the world experts',
         image: 'https://picsum.photos/200/200'
     } ],
 
@@ -43,32 +73,64 @@ const state = {
         role: 'Community Manager',
         image: 'https://picsum.photos/200/200'
     }, {
-        name: 'Shad Roi de la Cruz',
-        role: 'Community Manager',
+        name: 'Frances Marie Teves',
+        role: '',
         image: 'https://picsum.photos/200/200'
     }, {
-        name: 'Shad Roi de la Cruz',
-        role: 'Community Manager',
+        name: 'Emelie Diamante Anggoy',
+        role: '',
         image: 'https://picsum.photos/200/200'
     }, {
-        name: 'Shad Roi de la Cruz',
-        role: 'Community Manager',
+        name: 'Kim Bismark',
+        role: '',
         image: 'https://picsum.photos/200/200'
     }, {
-        name: 'Shad Roi de la Cruz',
-        role: 'Community Manager',
+        name: 'Franchette Camoro',
+        role: '',
         image: 'https://picsum.photos/200/200'
     }, {
-        name: 'Shad Roi de la Cruz',
-        role: 'Community Manager',
+        name: 'Julia Menchavez',
+        role: '',
         image: 'https://picsum.photos/200/200'
     }, {
-        name: 'Shad Roi de la Cruz',
-        role: 'Community Manager',
+        name: 'Rome Nicolas',
+        role: '',
         image: 'https://picsum.photos/200/200'
     }, {
-        name: 'Shad Roi de la Cruz',
-        role: 'Community Manager',
+        name: 'Maurice Jitty Marimon Villaester',
+        role: '',
+        image: 'https://picsum.photos/200/200'
+    }, {
+        name: 'Ayrton Wyn Chrysler Adiong',
+        role: '',
+        image: 'https://picsum.photos/200/200'
+    }, {
+        name: 'Francis Jigssy Galua',
+        role: '',
+        image: 'https://picsum.photos/200/200'
+    }, {
+        name: 'Rhodchel Lhuven Marie Española',
+        role: '',
+        image: 'https://picsum.photos/200/200'
+    }, {
+        name: 'Mark Robin',
+        role: '',
+        image: 'https://picsum.photos/200/200'
+    }, {
+        name: 'Christian Alferez',
+        role: '',
+        image: 'https://picsum.photos/200/200'
+    }, {
+        name: 'Ryan Viajedor',
+        role: '',
+        image: 'https://picsum.photos/200/200'
+    }, {
+        name: 'Emmanuel Lodovice',
+        role: '',
+        image: 'https://picsum.photos/200/200'
+    }, {
+        name: 'Kevin Calingacion',
+        role: '',
         image: 'https://picsum.photos/200/200'
     } ]
 };
