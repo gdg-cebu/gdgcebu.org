847a4444e17759cc25524ed74f1a84953278c2ea
Arnelle Balane <arnellebalane@gmail.com>
2019-10-01 20:36:53 +0800

    Add TheHeader component and global styles

--- source/App.vue
+++ source/App.vue
@@ -1,3 +1,41 @@
 <template>
-    <router-view></router-view>
+    <div>
+        <TheHeader />
+        <router-view></router-view>
+    </div>
 </template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+
+export default {
+    name: 'App',
+
+    components: {
+        TheHeader
+    }
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Google+Sans&display=swap");
+
+*,
+*::before,
+*::after {
+    box-sizing: border-box;
+    margin: 0;
+}
+
+html {
+    font-size: 62.5%;
+}
+
+body {
+    font-family: "Google Sans", sans-serif;
+    font-size: 1.8rem;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #555;
+}
+</style>
---
+++ source/components/TheHeader.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>GDG Cebu</h1>
+</template>
+
+<script>
+export default {
+    name: 'TheHeader'
+};
+</script>
