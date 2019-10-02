db8446c3249ddaad0a04a4b0fea4dcafa32699b6
Arnelle Balane <arnellebalane@gmail.com>
2019-10-02 22:48:28 +0800

    Make FeaturedEventSection component responsive

--- source/components/FeaturedEventSection.vue
+++ source/components/FeaturedEventSection.vue
@@ -7,12 +7,17 @@
                 <h2>GDG DevFest Cebu 2019</h2>
                 <p>DevFest 2019 brings together world-class experts in Android, Web, and Cloud technologies  for a day of sessions, workshops, and showcases.</p>
 
-                <ul>
-                    <li>November 16, 2019</li>
-                    <li>J Centre Mall, Mandaue City</li>
-                </ul>
+                <div class="details">
+                    <ul>
+                        <li>November 16, 2019</li>
+                        <li>J Centre Mall, Mandaue City</li>
+                    </ul>
 
-                <a href="#">Learn more</a>
+                    <a href="#">
+                        <img src="~images/arrow.svg" alt="">
+                        Learn more
+                    </a>
+                </div>
             </article>
         </div>
 
@@ -57,28 +62,55 @@ article p {
     margin-bottom: 2.4rem;
 }
 
-article a {
-    display: inline-block;
+.details {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    margin-bottom: -3.2rem;
+}
+
+.details > * {
+    margin-bottom: 3.2rem;
+}
+
+.details ul {
+    margin-right: 3.2rem;
+}
+
+.details a {
+    display: inline-flex;
+    align-items: center;
     padding: 0 2.4rem;
-    margin-top: 2.4rem;
+    margin-left: auto;
 
     line-height: 4rem;
     color: var(--primary-text-color);
     background-color: var(--gdg-yellow);
 }
 
+.details img {
+    width: 2rem;
+    margin-right: 1.8rem;
+}
+
 .image {
-    display: none;
+    margin: 0 -3.2rem;
+}
+
+.image img {
+    display: block;
+    width: 100%;
 }
 
 @media (min-width: 576px) {
     section {
-        padding: 0 5rem;
+        padding: 5rem;
+        padding-top: 0;
     }
 
     .content {
-        padding: 6rem;
-        padding-top: 0;
+        padding: 0 6rem;
+        padding-bottom: 3.2rem;
     }
 
     h1 {
@@ -100,13 +132,70 @@ article a {
         margin-bottom: 3.2rem;
     }
 
-    article li {
+    .details li {
         margin-bottom: 8px;
         font-size: 1.6rem;
     }
 
-    article a {
+    .details a {
         line-height: 4.8rem;
+    }
+
+    .details img {
+        width: 2.4rem;
+    }
+
+    .image {
+        margin: 0;
+        position: relative;
+    }
+
+    .image::before {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: -1;
+        transform: translate(-3.2rem, 3.2rem);
+
+        background: url("~images/dot.svg") repeat;
+    }
+}
+
+@media (min-width: 730px) {
+    section {
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-start;
+        flex-wrap: wrap;
+
+        max-width: 145rem;
+        margin: 0 auto;
+    }
+
+    .content {
+        flex: 1;
+        min-width: 45rem;
+        max-width: 55rem;
+
+        margin-right: 10rem;
+    }
+
+    .details a {
+        margin-right: -11rem;
+    }
+
+    .image {
+        flex: 1;
+        min-width: 40rem;
+        max-width: 62rem;
+        margin-left: auto;
+    }
+
+    .image::before {
+        transform: translate(-5rem, 5rem);
     }
 }
 </style>
